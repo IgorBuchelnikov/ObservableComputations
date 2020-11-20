3d038d16299d4963077d2df60237dc7cd7e6b702
Igor Buchelnikov <igor_buchelnikov_github@mail.ru>
2020-11-20 16:42:51 +0300

    work

---
+++ src/ObservableComputations/Common/Interface/ICollectionProcessing.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ObservableComputations
+{
+	public interface ICollectionProcessing : ICollectionComputing
+	{
+        bool Initializing { get; }
+	}
+
+}

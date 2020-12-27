e37afc37fbc86dd02e2a596ba042f8ff0d37d537
Igor Buchelnikov <igor_buchelnikov_github@mail.ru>
2020-12-27 15:18:47 +0300

    works

---
+++ src/ObservableComputations/HelperExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ObservableComputations
+{
+	public static class HelperExtensionMethods
+	{
+		[ObservableComputationsCall]
+		public static TComputation SetStackTraceToDebugTag<TComputation>(this TComputation computation)
+			where TComputation : IHasTags
+		{
+			computation.DebugTag = Environment.StackTrace;
+			return computation;
+		}
+	}
+}

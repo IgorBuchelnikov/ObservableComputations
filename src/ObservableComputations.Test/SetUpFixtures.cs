43934e8a9991642cfbf08eef80a276bd9ce23698
Igor Buchelnikov <igor_buchelnikov_github@mail.ru>
2021-01-06 22:03:46 +0300

    works

---
+++ src/ObservableComputations.Test/SetUpFixtures.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ObservableComputations.Test
+{
+	[NonParallelizable]
+	[SetUpFixture]
+	public class SetUpFixture
+	{
+		public SetUpFixture()
+		{
+			Configuration.SaveInstantiatingStackTrace = false;
+			Configuration.TrackComputingsExecutingUserCode = false;
+			Configuration.SaveOcDispatcherInvocationStackTrace = false;
+			Configuration.TrackOcDispatcherInvocations = false;
+		}
+	}
+
+	[NonParallelizable]
+	[SetUpFixture]
+	public class SetUpFixtureDebug
+	{
+		public SetUpFixtureDebug()
+		{
+			Configuration.SaveInstantiatingStackTrace = true;
+			Configuration.TrackComputingsExecutingUserCode = true;
+			Configuration.SaveOcDispatcherInvocationStackTrace = true;
+			Configuration.TrackOcDispatcherInvocations = true;
+		}
+	}
+}
